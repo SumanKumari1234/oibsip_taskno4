540b04daa08d1db9b70516166043de3000038774
SumanKumari1234 <149669033+SumanKumari1234@users.noreply.github.com>
2023-11-13 11:41:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const username=document.querySelector('#username');
+const password=document.querySelector('#password');
+const submit=document.querySelector('#submit');
+submit.addEventListener('click',(event)=>{
+    event.preventDefault();
+    if (username.value.trim()==''){
+        error(username,'username can not be empty');
+
+    }else{
+        success(username);
+    }
+    if (password.value.trim()==''){
+        error(password,'username can not be empty');
+    }else{
+        success(password);
+    }
+
+});
+function error(element,msg){
+    element.style.border='3px red solid';
+    const parent=element.parentElement;
+    const p=parent.querySelector('p');
+    p.textContent=msg;
+    p.style.visibility='visible';
+}
+function success(element){
+    element.style.border='3px green solid';
+    const parent=element.parentElement;
+    const p=parent.querySelector('p');
+   
+    p.style.visibility='hidden'; 
+}
+    
